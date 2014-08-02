3a5f581ddc27ec03bab8e88c2125ea37955892f7
Marcin Czołnowski <marcin@czolnowski.net>
2014-08-02 13:28:17 +0200

    Change colors to more different.

--- app/scripts/widget/module.js
+++ app/scripts/widget/module.js
@@ -5,70 +5,88 @@
         $provide.value(
             'chartsColors', [
                 {
-                    // #56baec
-                    fillColor: "rgba(86,186,236,0.2)",
-                    strokeColor: "rgba(86,186,236,1)",
-                    pointColor: "rgba(86,186,236,1)",
+                    // 113F8C
+                    // 17/63/140
+                    fillColor: "rgba(17,63,140,0.2)",
+                    strokeColor: "rgba(17,63,140,1)",
+                    pointColor: "rgba(17,63,140,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(86,186,236,1)",
-                    color: "#56baec",
-                    highlight: "#56baec"
+                    pointHighlightStroke: "rgba(17,63,140,1)",
+                    color: "#113F8C",
+                    highlight: "#113F8C"
                 },
                 {
-                    // 216BC0
-                    fillColor: "rgba(33,107,192,0.2)",
-                    strokeColor: "rgba(33,107,192,1)",
-                    pointColor: "rgba(33,107,192,1)",
+                    // 01A4A4
+                    // 1/164/164
+                    fillColor: "rgba(1,164,164,0.2)",
+                    strokeColor: "rgba(1,164,164,1)",
+                    pointColor: "rgba(1,164,164,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(33,107,192,1)",
-                    color: "#216BC0",
-                    highlight: "#216BC0"
+                    pointHighlightStroke: "rgba(1,164,164,1)",
+                    color: "#01A4A4",
+                    highlight: "#01A4A4"
                 },
                 {
-                    // 184E8C
-                    fillColor: "rgba(24,78,140,0.2)",
-                    strokeColor: "rgba(24,78,140,1)",
-                    pointColor: "rgba(24,78,140,1)",
+                    // 00A1CB
+                    // 0/161/203
+                    fillColor: "rgba(0,161,203,0.2)",
+                    strokeColor: "rgba(0,161,203,1)",
+                    pointColor: "rgba(0,161,203,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(24,78,140,1)",
-                    color: "#184E8C",
-                    highlight: "#184E8C"
+                    pointHighlightStroke: "rgba(0,161,203,1)",
+                    color: "#00A1CB",
+                    highlight: "#00A1CB"
                 },
                 {
-                    // 15447A
-                    fillColor: "rgba(21,68,122,0.2)",
-                    strokeColor: "rgba(21,68,122,1)",
-                    pointColor: "rgba(21,68,122,1)",
+                    // 61AE24
+                    // 97/174/36
+                    fillColor: "rgba(97,174,36,0.2)",
+                    strokeColor: "rgba(97,174,36,1)",
+                    pointColor: "rgba(97,174,36,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(21,68,122,1)",
-                    color: "#15447A",
-                    highlight: "#15447A"
+                    pointHighlightStroke: "rgba(97,174,36,1)",
+                    color: "#61AE24",
+                    highlight: "#61AE24"
                 },
                 {
-                    // 123A69
-                    fillColor: "rgba(18,58,105,0.2)",
-                    strokeColor: "rgba(18,58,105,1)",
-                    pointColor: "rgba(18,58,105,1)",
+                    // D0D102
+                    // 208/209/2
+                    fillColor: "rgba(208,209,2,0.2)",
+                    strokeColor: "rgba(208,209,2,1)",
+                    pointColor: "rgba(208,209,2,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(18,58,105,1)",
-                    color: "#123A69",
-                    highlight: "#123A69"
+                    pointHighlightStroke: "rgba(208,209,2,1)",
+                    color: "#D0D102",
+                    highlight: "#D0D102"
                 },
                 {
-                    // 0F3157
-                    fillColor: "rgba(15,49,87,0.2)",
-                    strokeColor: "rgba(15,49,87,1)",
-                    pointColor: "rgba(15,49,87,1)",
+                    // 32742C
+                    // 50/116/44
+                    fillColor: "rgba(50,116,44,0.2)",
+                    strokeColor: "rgba(50,116,44,1)",
+                    pointColor: "rgba(50,116,44,1)",
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(15,49,87,1)",
-                    color: "#0F3157",
-                    highlight: "#0F3157"
+                    pointHighlightStroke: "rgba(50,116,44,1)",
+                    color: "#32742C",
+                    highlight: "#32742C"
+                },
+                {
+                    // D70060
+                    // 215/0/96
+                    fillColor: "rgba(215,0,96,0.2)",
+                    strokeColor: "rgba(215,0,96,1)",
+                    pointColor: "rgba(215,0,96,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(215,0,96,1)",
+                    color: "#D70060",
+                    highlight: "#D70060"
                 }
             ]
         );
