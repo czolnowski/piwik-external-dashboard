8b51891b760e1f3afb4a4f166d94a49726780bed
Marcin Czołnowski <marcin@czolnowski.net>
2014-08-18 00:48:02 +0200

    Add parsing embedded values for evolution graph.

--- app/scripts/morris/directives/EvolutionVisualization.js
+++ app/scripts/morris/directives/EvolutionVisualization.js
@@ -4,8 +4,8 @@
     ng.module('piwikExtDash.morris').directive(
         'morrisEvolutionVisualization',
         [
-            "MetricsService",
-            function (MetricsService)
+            "MetricsService", "Metric",
+            function (MetricsService, Metric)
             {
                 return {
                     require: '^singleWidget',
@@ -25,17 +25,58 @@
                                 );
 
                                 if (ng.isDefined(response.data.reportData)) {
-                                    $scope.data = MetricsService.parseValues(
-                                        response.data.reportData,
-                                        $scope.ykeys,
-                                        $scope.labels,
-                                        $scope.metrics,
-                                        $scope.xkey,
-                                        function (values, key)
-                                        {
-                                            values[$scope.xkey] = new Date(key).getTime();
+                                    if (ng.isObject(response.data.reportData[0])) {
+                                        console.log('nie ma transformacji', response.data.reportData)
+                                        $scope.data = MetricsService.parseValues(
+                                            response.data.reportData,
+                                            $scope.ykeys,
+                                            $scope.labels,
+                                            $scope.metrics,
+                                            $scope.xkey,
+                                            function (values, key)
+                                            {
+                                                values[$scope.xkey] = new Date(key).getTime();
+                                            }
+                                        );
+                                    } else {
+                                        var firstLoop = true,
+                                            keyForValues = $scope.metrics[0].key,
+                                            numberOfMetrics = 0;
+                                        for (var key in response.data.reportData) {
+                                            if (response.data.reportData.hasOwnProperty(key)) {
+                                                var values = response.data.reportData[key],
+                                                    valuesForData = {};
+
+                                                if (firstLoop) {
+                                                    numberOfMetrics = Math.min(10, values.length);
+                                                    firstLoop = false;
+
+                                                    $scope.ykeys = [];
+                                                    $scope.metrics = [];
+                                                    $scope.labels = [];
+
+                                                    for (var i = 0; i < numberOfMetrics; ++i) {
+                                                        $scope.ykeys.push('metric' + i);
+                                                        $scope.labels.push(values[i].label);
+                                                        $scope.metrics.push(
+                                                            new Metric(
+                                                                values[i].label,
+                                                                'metric' + i,
+                                                                ""
+                                                            )
+                                                        );
+                                                    }
+                                                }
+
+                                                for (var i = 0; i < numberOfMetrics; ++i) {
+                                                    valuesForData['metric' + i] = values[i][keyForValues];
+                                                }
+
+                                                valuesForData[$scope.xkey] = new Date(key).getTime();
+                                                $scope.data.push(valuesForData);
+                                            }
                                         }
-                                    );
+                                    }
                                 }
 
                                 MetricsService.rebuild($scope.metrics);
