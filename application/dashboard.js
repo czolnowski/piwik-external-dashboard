55232ee5e090692d4763644a6b2a80e464beebd3
Marcin Czołnowski <marcin@czolnowski.net>
2014-12-22 14:18:25 +0100

    Remove winston from dependencies.

--- application/dashboard.js
+++ application/dashboard.js
@@ -6,24 +6,20 @@ var app = require('connect')(),
     configRoute = require('./config/index'),
     serveStatic = require('serve-static'),
     fs = require('fs'),
-    http = require('http'),
-    winston = require('winston'),
-    log = new winston.Logger();
+    http = require('http');
 
 module.exports = function (serverConfig, frontendConfig) {
     if (typeof serverConfig.static !== 'string') {
-        log.log('info', 'config.static set to "app".');
         serverConfig.static = 'app';
     }
 
     if (typeof serverConfig.port !== 'number') {
-        log.log('info', 'config.port set to "3000".');
         serverConfig.port = 3000;
     }
 
     app.use(apiRoute.path, apiRoute.resolver(serverConfig, frontendConfig));
     app.use(configRoute.path, configRoute.resolver(serverConfig, frontendConfig));
-    app.use(serveStatic(serverConfig.static, {}))
+    app.use(serveStatic(serverConfig.static, {}));
 
     app.use(function (request, response) {
         fs.readFile(serverConfig.static + '/index.html', "binary", function (err, file) {
@@ -42,7 +38,6 @@ module.exports = function (serverConfig, frontendConfig) {
     });
 
     http.createServer(app).listen(serverConfig.port);
-    log.log('info', 'Server is running.');
 };
 
 
